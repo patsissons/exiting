b4cd66debeb4605c0c2aa02662f3a5742b796592
Pat Sissons <patricksissons@gmail.com>
2022-12-18 16:20:46 -0800

    cleaning up some unused packages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,11 @@
 import "styles/globals.css";
-import { Session } from "@supabase/auth-helpers-react";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import { PolarisProvider } from "components/PolarisProvider";
 import { ToastsProvider } from "components/ToastsProvider";
 import Head from "next/head";
 
-export interface Props {
-  initialSession: Session;
-}
-
-export default function App({ Component, pageProps }: AppProps<Props>) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <PolarisProvider>
