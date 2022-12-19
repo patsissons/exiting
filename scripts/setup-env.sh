6a4f6fbf5bc2fac215758701fc2750f3ad98c19f
Pat Sissons <patricksissons@gmail.com>
2022-12-19 12:22:37 -0800

    improving env setup

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -2,6 +2,8 @@
 #!/bin/bash
 
 # .env.local
-if [ ! -r ".env.local" ]; then
-  yarn run vercel env pull .env.local && sed -i '' -e '/^VERCEL/d' .env.local
+if [ ! -r ".env.local" ] || [ "${1}" == "-f" ]; then
+  yarn run vercel env pull -y .env.local && \
+  sed -i '' -e '/^VERCEL/d' .env.local && \
+  yarn run supabase status -o env --override-name api.url=SUPABASE_URL --override-name auth.service_role_key=SUPABASE_SERVICE_KEY | grep '^\(SUPABASE_URL\|SUPABASE_SERVICE_KEY\)' >> .env.local
 fi
