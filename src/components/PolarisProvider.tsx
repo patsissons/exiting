c81c87652c4534fe3e7511ed742e3b0097d9c875
Pat Sissons <patricksissons@gmail.com>
2022-12-19 19:42:43 -0800

    implementing `next/link`

--- src/components/PolarisProvider.tsx
+++ src/components/PolarisProvider.tsx
@@ -1,8 +1,21 @@
 import { AppProvider } from "@shopify/polaris";
 import "@shopify/polaris/build/esm/styles.css";
 import enTranslations from "@shopify/polaris/locales/en.json";
+import NextLink from "next/link";
 import { PropsWithChildren } from "react";
 
 export function PolarisProvider({ children }: PropsWithChildren) {
-  return <AppProvider i18n={enTranslations}>{children}</AppProvider>;
+  return (
+    <AppProvider i18n={enTranslations} linkComponent={Link}>
+      {children}
+    </AppProvider>
+  );
+}
+
+function Link({ children, url, ...props }: PropsWithChildren<{ url: string }>) {
+  return (
+    <NextLink {...props} href={url}>
+      {children}
+    </NextLink>
+  );
 }
